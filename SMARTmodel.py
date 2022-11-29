a0f37d50035d25b4a0064180eae90c17e3a78801
Masaya Sato <masayasato0407@gmail.com>
2022-11-29 12:15:11 +0900

    Update SMARTmodel.py

--- SMARTmodel.py
+++ SMARTmodel.py
@@ -77,14 +77,11 @@ st.header("HCC risk for submitted patient")
 st.image ("img.png")
 
 if rfscore < 0.956: 
-    st.subheader("Risk grouping for HCC in the original article: Low risk")
-    st.markdown("HCC incidence in the low-risk group of the original study cohort: 0/1000 person-year (95%CI:0-2/1000)")
+    st.markdown("Risk grouping for HCC in the original article: Low risk")
 elif rfscore >= 3.20: 
-    st.subheader("Risk grouping for HCC in the original article: High risk")
-    st.markdown("HCC incidence in the high-risk group of the original study cohort: 15 /1000 person-year (95%CI: 11-22/1000)")
+    st.markdown("Risk grouping for HCC in the original article: High risk")
 else:
-    st.subheader("Risk grouping for HCC in the original article: Intermediate risk")
-    st.markdown("HCC incidence in the intermediate-risk group of the original study cohort: 3/1000 person-year (95%CI:1-6/1000)")
+    st.markdown("Risk grouping for HCC in the original article: Intermediate risk")
 
 y_pred = rsf.predict(X).flatten()[0]
 y_event = rsf.predict_survival_function(X, return_array=True).flatten()
